39440d61464b985f0517e65e6714d08ff029277e
Moyan Xiao <xiaomoyan@gmail.com>
2018-02-21 12:40:56 +0800

    script for ss

---
+++ shell/config-ss.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+if [ $USER != 'root' ] then
+    echo 'should be run in ROOT/SUDO'
+    exit 1
+fi
+
+apt-get install git shadowsocks-libev
+
+git clone https://github.com/shadowsocks/simple-obfs.git
+cd simple-obfs
+git submodule update --init --recursive
+./autogen.sh
+./configure --disable-documentation
+make && make install
+
+
