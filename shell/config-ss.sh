91401bf516c889131b8387e7b0634c316f9b0fe0
Your Name <you@example.com>
2018-06-01 19:23:34 +0000

    autoconfig

--- shell/config-ss.sh
+++ shell/config-ss.sh
@@ -6,7 +6,7 @@ then
     exit 1
 fi
 
-apt-get install git shadowsocks-libev autoconf
+apt-get install git shadowsocks-libev autoconf libtool libsysfs-dev libev-dev -y
 
 git clone https://github.com/shadowsocks/simple-obfs.git
 cd simple-obfs
