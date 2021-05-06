d58d9ad25583c26d933edebb0b9d0e0bd4ebeb1c
Moyan Xiao <xiaomoyan@gmail.com>
2021-05-06 21:58:40 +0000

    dircolor link

--- shell/config-zsh.sh
+++ shell/config-zsh.sh
@@ -9,9 +9,6 @@ fi
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-if [ -f ~/.zshrc ] 
-then
-    mv ~/.zshrc ~/.zshrc.bak
-fi
 
-ln -s ~/VimShell/shell/zshrc.zsh-template ~/.zshrc
+ln -s -f ~/VimShell/shell/zshrc.zsh-template ~/.zshrc
+ln -s -f ~/VimShell/shell/dircolors ~/.dircolors
